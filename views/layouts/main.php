c34f8a8ee34c7a69891b87a17302322b6258c5dc
Nastya <anastasion0107@gmail.com>
2025-05-04 01:56:11 +0700

    login layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -7,27 +7,93 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pop it MVC</title>
 </head>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    .header {
+        background: #D9D9D9;
+    }
+    .container{
+        max-width: 1660px;
+        margin: 0 auto;
+    }
+    .nav {
+        display: flex;
+        justify-content: space-between;
+        padding: 28px 0;
+    }
+    .logo {
+        font-size: 24px;
+    }
+    .nav-list__item {
+        font-size: 20px;
+    }
+    .form-input {
+        padding: 4px 2px;
+    }
+    .form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+        max-width: 300px;
+    }
+    .form-label {
+        display: flex;
+        flex-direction: column;
+        font-size: 20px;
+    }
+    .login {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+        padding: 100px 0;
+    }
+    .login-title {
+        font-size: 36px;
+    }
+    .login-subtitle {
+        font-size: 36px;
+    }
+    .button {
+        padding: 8px 40px;
+        display: flex;
+    }
+</style>
 <body>
 
-<header>
-    <nav>
-        <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
-        <?php
-        if (!app()->auth::check()):
-            ?>
-            <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
-            <a href="<?= app()->route->getUrl('/signup') ?>">Регистрация</a>
-        <?php
-        else:
-            ?>
-            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
-        <?php
-        endif;
-        ?>
-    </nav>
+<header class="header">
+    <div class="container">
+        <nav class="nav">
+            <a href="<?= app()->route->getUrl('/hello') ?>" class="logo">ПОЛИКЛИННИКА.СИСТЕМА</a>
+            <div class="nav-list">
+                <?php
+                if (!app()->auth::check()):
+                    ?>
+                    <a href="<?= app()->route->getUrl('/login') ?>" class="nav-list__item">ВХОД</a>
+                <?php
+                else:
+                    ?>
+                    <a href="<?= app()->route->getUrl('/logout') ?>" class="nav-list__item">ВЫЙТИ ИЗ АККАУНТА</a>
+                <?php
+                endif;
+                ?>
+            </div>
+        </nav>
+    </div>
 </header>
 <main>
-    <?= $content ?? '' ?>
+    <div class="container">
+        <?= $content ?? '' ?>
+    </div>
 </main>
 
 
--- views/site/login.php
+++ views/site/login.php
@@ -1,13 +1,17 @@
-<h2>Авторизация</h2>
-<h3><?= $message ?? ''; ?></h3>
+<div class="login">
+    <h2 class="login-title">Система для регистратуры поликлинники</h2>
+    <h3 class="login-subtitle">Чтобы получить доступ к фукнционалу, войдите в аккаунт</h3>
+    <h3><?= $message ?? ''; ?></h3>
 
-<h3><?= app()->auth->user()->name ?? ''; ?></h3>
-<?php
-if (!app()->auth::check()):
+    <h3><?= app()->auth->user()->name ?? ''; ?></h3>
+    <?php
+        if (!app()->auth::check()):
+            ?>
+            <form method="post" class="form">
+                <label class="form-label">Логин <input class="form-input" type="text" name="login"></label>
+                <label class="form-label">Пароль <input class="form-input" type="password" name="password"></label>
+                <button class="button">Войти</button>
+            </form>
+        <?php endif;
     ?>
-    <form method="post">
-        <label>Логин <input type="text" name="login"></label>
-        <label>Пароль <input type="password" name="password"></label>
-        <button>Войти</button>
-    </form>
-<?php endif;
+</div>
