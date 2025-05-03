d637211cf3cff8beb172d6f64aba37ca5703033f
Nastya <anastasion0107@gmail.com>
2025-05-03 19:51:15 +0700

    middleware func

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -21,6 +21,11 @@ class Site
         return new View('site.hello', ['message' => 'hello working']);
     }
 
+    public function pipiska()
+    {
+        $view = new View('site.pipiska');
+    }
+
     public function signup(Request $request): string
     {
         if ($request->method==='POST' && User::create($request->all())){
