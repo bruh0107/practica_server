b2857bf00c36ff4ee079e44351ce0eef9e828595
Nastya <anastasion0107@gmail.com>
2025-05-03 18:09:42 +0700

    register, auth, logout functional

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,8 @@ namespace Controller;
 use Model\Post;
 use Src\View;
 use Src\Request;
+use Model\User;
+use Src\Auth\Auth;
 
 class Site
 {
@@ -18,4 +20,32 @@ class Site
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            app()->route->redirect('/go');
+        }
+        return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        if($request->method === 'GET'){
+            return new View('site.login');
+        }
+
+        if(Auth::attempt($request->all())){
+            app()->route->redirect('/hello');
+        }
+
+        return new View('site.login', ['message' => 'Неверные логин или пароль']);
+    }
+
+    public function logout(Request $request): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
+    }
+
 }
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,6 +2,13 @@
 const DIR_CONFIG = '/../config';
 
 require_once __DIR__ . '/../vendor/autoload.php';
+$app = new Src\Application(new Src\Settings(getConfigs()));
+
+function app()
+{
+    global $app;
+    return $app;
+}
 
 function getConfigs(string $path = DIR_CONFIG): array
 {
@@ -17,4 +24,4 @@ function getConfigs(string $path = DIR_CONFIG): array
 
 require_once __DIR__ . '/../routes/web.php';
 
-return new Src\Application(new Src\Settings(getConfigs()));
+return $app;
